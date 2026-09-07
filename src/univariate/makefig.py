93709b4e17ece843ac17ce089ba728b2099c77bf
Minazuki-666 <mi7zuki666@gmail.com>
2026-09-07 16:56:52 +0900

    secでの差異を追加

--- src/univariate/makefig.py
+++ src/univariate/makefig.py
@@ -5,6 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from statsmodels.stats.multicomp import pairwise_tukeyhsd
+from statsmodels.stats.weightstats import ttest_ind
 
 sns.set_theme(style="white")
 sns.set_context("talk", font_scale=1.2)
@@ -93,4 +94,74 @@ for metabolite in df.columns[5:]:
     os.path.join(heredir, "fig", "timingStats", f"{metabolite}.svg"),
     bbox_inches="tight"
   )
+
+# %%
+for metabolite in df.columns[5:]:
+  fig, ax = plt.subplots(figsize=(6, 6))
+  sns.stripplot(
+    data=df, 
+    x="timing",
+    y=metabolite,
+    hue="sec",
+    dodge=True,
+    palette="deep",
+  alpha=0.4,
+  ax=ax
+  )
+  sns.boxplot(
+    data=df,
+    x="timing",
+    y=metabolite,
+    hue="sec",
+    dodge=True,
+    showfliers=False,
+    palette="deep",
+    fill=False,
+    legend=False,
+    ax=ax
+  )
+  height_original = ax.get_ylim()[1]
+
+  for i in df["timing"].unique():
+    ttest_results = ttest_ind(
+      df[(df["timing"] == i) & (df["sec"] == True)][metabolite],
+      df[(df["timing"] == i) & (df["sec"] == False)][metabolite],
+      usevar="unequal"
+    )
+    pval = ttest_results[1]
+    if pval < 0.001:
+      pval_str = "p < 0.001"
+    else:
+      pval_str = f"p = {pval:.3f}"
+
+    if pval < 0.05:
+      ax.text(
+        x=i,
+        y=height_original,
+        s=pval_str,
+        fontsize=16,
+        ha="center",
+        va="bottom"
+      )
+      ax.hlines(
+        y=height_original,
+        xmin=i - 0.2,
+        xmax=i + 0.2,
+        color="black",
+        linewidth=1
+      )
+
+  ax.set_ylim(top=height_original * 1.1)
+  ax.set_xticks([0, 1, 2])
+  ax.set_xticklabels(["Newborn", "1 mo.", "4-5 mos."])
+  ax.set_xlabel("")
+  ax.set_ylabel("Concentration (mM)")
+  ax.set_title(metabolite)
+  ax.legend(["Se+", "Se-"], title="")
+  os.makedirs(os.path.join(heredir, "fig", "secStats"), exist_ok=True)
+  plt.savefig(
+    os.path.join(heredir, "fig", "secStats", f"{metabolite}.svg"),
+    bbox_inches="tight"
+  )
+
 # %%
